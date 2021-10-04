fe5e50667dde375c25a74a794e01b19dd8ffbd60
HaileyJung <hyunchen81@gmail.com>
2021-10-04 23:42:32 +0900

    두번째

---
+++ back.js
@@ -0,0 +1,7 @@
+let today = new Date();
+
+let year = today.getFullYear();
+let month = today.getMonth()+1;
+let date = today.getDate();
+
+let 날짜 = document.write('오늘의 날짜 : '+ year +'년'+month+'월'+date+'일')
